c5e025f00b801dbbbce7cd60707c823fcb3df8b5
Suraj Shenoy <suraj.shenoy.b@gmail.com>
2023-05-28 16:00:54 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Login } from "./Login";
-import { Register } from "./Register";
+import Navbar from "./components/Navbar";
+import Home from './components/Home';
 
 function App() {
 
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
   return (
-    <div className="App">
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
+    <div>
+      <Navbar />
+
+      <div className="App">
+
+        <h1>WELCOME TO PERSONALITY!!!!!</h1>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Careers from './components/Careers';
+import About from './components/About';
+import Contact from './components/Contact';
+import Login from './components/Login';
+import Register from './components/Register'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path="/careers" element={<Careers/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/Login" element={<Login/>}/>
+      <Route path="/Register" element={<Register/>}/>
+    </Routes>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
